2311ad86c2ae64f49743a40c5d758f04d3b3b4c5
Самигуллин Тимур Рашитович <t.samigullin@rlsnet.ru>
2025-03-30 22:21:39 +0300

    add exceptions handling

--- docker/app.sh
+++ docker/app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sleep 3 # Wait for the database to be ready
 
 alembic upgrade head
 
--- src/shurl/router.py
+++ src/shurl/router.py
@@ -1,34 +1,54 @@
 from fastapi import APIRouter, HTTPException, Depends, status, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import insert
+from sqlalchemy.exc import IntegrityError
+
 from typing_extensions import Annotated
 from datetime import datetime
 from shurl.models import ShortenedItem
 from database import get_async_session
 from shurl.utils import generate_random_string
 from shurl.schemas import Link
+from logging import getLogger
+
+logger = getLogger('shurl_router')
 
 router = APIRouter(
     prefix="/links",
     tags=["Links"],
 )
 
-# POST /links/shorten – создает короткую ссылку.
 @router.post("/shorten", status_code=status.HTTP_201_CREATED)
 async def shorten_link(original_url: Annotated[str, Query()],
                        session: Annotated[AsyncSession, Depends(get_async_session)],
                        custom_alias: Annotated[str | None, Query()] = None,
                        expires_at: Annotated[datetime | None, Query()] = None):
+    """Создает короткую ссылку."""
     try:
         if custom_alias is not None:
             short_code = custom_alias
         else:
             short_code = generate_random_string()
-        shurl = ShortenedItem(short_url=short_code, original_url=original_url, expires_at=expires_at)
-        statement = insert(Link).values(**shurl.model_dump())
-        await session.execute(statement)
-        await session.commit()
+
+        while True:
+            shurl = ShortenedItem(short_url=short_code, original_url=original_url, expires_at=expires_at)
+            statement = insert(Link).values(**shurl.model_dump())
+            try:
+                await session.execute(statement)
+                await session.commit()
+                break
+            except IntegrityError:
+                await session.rollback()
+                logger.debug("Collision occurred, retrying with new short code.")
+
+                if custom_alias is None:
+                    short_code = generate_random_string()
+                else:
+                    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Custom alias already exists")
+
         return {"short_url": short_code}
+    except HTTPException as http_exc:
+        raise http_exc
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
