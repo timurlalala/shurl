4cce1fa111b4e690c134009118b2b3c3a9e7ef4b
Самигуллин Тимур Рашитович <t.samigullin@rlsnet.ru>
2025-03-31 12:59:23 +0300

    add delete handler

--- src/shurl/router.py
+++ src/shurl/router.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, HTTPException, Depends, status, Query, Path
 from fastapi.responses import RedirectResponse
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import insert, select
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy import insert, select, delete
+from sqlalchemy.exc import IntegrityError, NoResultFound, MultipleResultsFound
 
 from typing_extensions import Annotated
 from datetime import datetime
@@ -85,8 +85,27 @@ async def redirect_to_original(short_code: Annotated[str, Path(max_length=16)],
 
 # DELETE /links/{short_code} – удаляет связь.
 @router.delete("/{short_code}")
-async def delete_link(short_code: str):
-    pass
+async def delete_link(short_code: Annotated[str, Path(max_length=16)],
+                      session: Annotated[AsyncSession, Depends(get_async_session)]):
+    """Удаляет связь."""
+    try:
+        query = select(Link.__table__).where(Link.__table__.c.short_url == short_code) # type: ignore
+        result = await session.execute(query)
+        await session.commit()
+        try:
+            link = result.one()
+        except NoResultFound:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Short code not found")
+
+        delete_query = delete(Link.__table__).where(Link.__table__.c.short_url == short_code) # type: ignore
+        await session.execute(delete_query)
+        await session.commit()
+        return {"message": "Link deleted successfully"}
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
 
 
 # PUT /links/{short_code} – обновляет короткий адрес ссылки
